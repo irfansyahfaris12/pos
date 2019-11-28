5f39eaaad70f281e55b11add05ad3b4fc41e896e
irfansyahfaris12 <irfansyah.faris@gmail.com>
2019-11-28 08:35:09 +0700

    revisi1

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -7,10 +7,10 @@ export default function Routes() {
     return (
         <BrowserRouter>
             <Switch>
-                <Route exact path="/dashboard/harian" component={App} />
+                <Route  path="/dashboard/harian" component={App} />
                 <Route      path="/dashboard/bulanan" component={App} />
                 <Route      path="/transaksi" component={App} />
-                <Route      path="/login"     component={Login} />
+                <Route     exact path="/"     component={Login} />
             </Switch>
         </BrowserRouter>
     )
